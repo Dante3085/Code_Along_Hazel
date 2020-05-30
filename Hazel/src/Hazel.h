75287393cada71035039413129aef63a59a56050
mjsch <moritz97.s@web.de>
2020-05-30 12:58:19 +0200

    Integrated spdlog with Log Wrapper.

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -3,6 +3,7 @@
 // For use bz Hazel Application. The actual Hazel Library doesn't include this file ever.
 
 #include "Hazel/Application.h"
+#include "Hazel/Log.h"
 
 // Entry Point -------------------------------------------------
 #include "Hazel/EntryPoint.h"
