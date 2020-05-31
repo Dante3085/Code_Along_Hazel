c2c0740d5548eedec140ede1b591633e844dd2da
mjsch <moritz97.s@web.de>
2020-05-31 12:20:37 +0200

    using premake5 to build project files now.

---
+++ premake5.lua
@@ -0,0 +1,113 @@
+
+-- %{} is a Value Token. Depending on scope they have access to context objects: wks, prj, cfg and file.
+-- wks: current workspace
+-- prj: project
+-- cfg: configurations
+-- file: file configuration
+
+workspace "Hazel"      -- For us, this is the name of the Visual Studio solution.
+	architecture "x64" -- Platform in Visual Studio
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release", -- Faster version of Debug, but still with Logging. Still for development.
+		"Dist"     -- For distribution, no Logging.
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}" -- variable in lua
+
+project "Hazel"
+	location "Hazel" -- Everything in project Hazel is relative to this path now. location Hazel since this project is inside the Hazel folder in the solution folder.
+	kind "SharedLib" -- SharedLib = DLL
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}") -- "a .. b" appends string b to a.
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h", -- ** means recursively search
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+	
+	filter "system:windows" -- filters make it possible to use certain kinds of configurations only for specific platforms. In this case Windows.
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest" -- Windows SDK Version
+
+		defines
+		{
+			"HZ_PLATFORM_WINDOWS",
+			"HZ_BUILD_DLL"
+		}
+
+		postbuildcommands -- Automatic DLL placement after project build
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist" -- Dist := Distribution
+		defines "HZ_DIST"
+		symbols "On"
+
+project "Sandbox" -- These are all actually function calls, but parantheses can be omitted if only one argument is given
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}") -- "a .. b" appends string b to a.
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h", -- ** means recursively search
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Hazel/vendor/spdlog/include",
+		"Hazel/src"
+	}
+
+	links
+	{
+		"Hazel"
+	}
+	
+	filter "system:windows" -- filters make it possible to use certain kinds of configurations only for specific platforms. In this case Windows.
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest" -- Windows SDK Version
+
+		defines
+		{
+			"HZ_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "HZ_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "HZ_RELEASE"
+		symbols "On"
+
+	filter "configurations:Dist"
+		defines "HZ_DIST"
+		symbols "On"
