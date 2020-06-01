0b590b80ae1a6e2a94a0f578478dc1b1b88e144d
mjsch <moritz97.s@web.de>
2020-06-01 10:55:15 +0200

    Hazel Event System

--- Hazel/src/Hazel/Application.cpp
+++ Hazel/src/Hazel/Application.cpp
@@ -1,5 +1,8 @@
 #include "Application.h"
 
+#include "Hazel/Events/ApplicationEvent.h"
+#include "Hazel/Log.h"
+
 namespace Hazel
 {
 	Application::Application()
@@ -12,6 +15,17 @@ namespace Hazel
 
 	void Application::Run()
 	{
+		WindowResizeEvent e(1280, 720);
+		
+		if (e.IsInCategory(EventCategory::EventCategoryApplication))
+		{
+			HZ_CORE_ERROR("Is in EventCategoryApplication");
+		}
+		if (e.IsInCategory(EventCategory::EventCategoryInput))
+		{
+			HZ_CORE_ERROR("Is in EventCategoryInput");
+		}
+
 		while (true);
 	}
 }
--- Hazel/src/Hazel/Events/Event.h
+++ Hazel/src/Hazel/Events/Event.h
@@ -31,6 +31,14 @@ namespace Hazel
 		EventCategoryMouseButton   = BIT(4)
 	};
 
+// ##variable concatenates variable's value with the text left of it.
+// #variable puts string quotes around variable's value.
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EventType::##type; }\
+                               virtual EventType GetEventType() const override { return GetStaticType(); }\
+                               virtual const char* GetName() const override { return #type; }
+
+#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
+
 	class HAZEL_API Event
 	{
 		friend class EventDispatcher;
@@ -48,4 +56,35 @@ namespace Hazel
 	protected:
 		bool m_Handled = false;
 	};
+
+	class EventDispatcher
+	{
+		template <typename T>
+		using EventFn = std::function<bool(T&)>;
+	public:
+		EventDispatcher(Event& event)
+			: m_Event(event)
+		{
+
+		}
+
+		template <typename T>
+		bool Dispatch(EventFn<T> func)
+		{
+			if (m_Event.GetEventType() == T::GetStaticType())
+			{
+				m_Event.m_Handled = func(*(T*)&m_Event);
+				return true;
+			}
+			return false;
+		}
+
+	private:
+		Event& m_Event;
+	};
+
+	inline std::ostream& operator<<(std::ostream& os, Event const& e)
+	{
+		return os << e.ToString();
+	}
 }
